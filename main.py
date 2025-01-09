b27a28a829d81b1ab155d7d8d09836997d974bc2
liuyp2080 <78184640+liuyp2080@users.noreply.github.com>
2025-01-09 08:11:48 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -3,40 +3,60 @@ from pydantic import BaseModel
 from sklearn.preprocessing import StandardScaler
 import joblib
 import json
+import uvicorn
+import os
+import pandas as pd
+import numpy as np
+from dotenv import load_dotenv
 
-app = FastAPI()
+load_dotenv()
+root_path = os.getenv("SCRIPT_NAME", "")
+app = FastAPI(root_path=root_path)
 
 class model_input(BaseModel):
-    Pregancies: int
-    Glucose: int
-    BloodPressure: int
-    SkinThickness: int
-    Insulin: int
-    BMI: float
-    DiabetesPedigreeFunction: float
-    Age: int
+    fasting_plasma_glucose: float
+    family_history_of_diabetes: int
+    weight: float
+    cholesterol: float
+    blood_urea_nitrogen: float  
+    drinking_status: int
+    age: int
     
-diabetes_model = joblib.load("diabetes_model.sav")
+diabetes_model = joblib.load("rsf_best.pkl")
 
-@app.post('/diabetes_prediction')
-def diabetes_pred(input_parameters: model_input):
+@app.post('/diabetes_pred_time')
+def diabetes_pred_time(input_parameters: model_input):
     input_data = input_parameters.json()
     input_dictionary = json.loads(input_data)
+    patient = pd.DataFrame(input_dictionary,index=[0]) 
+    times= np.arange(3,5,1/6)
+    chf_funcs=diabetes_model.predict_cumulative_hazard_function(patient)
+    for fn in chf_funcs:
+        if fn(times[-1])<1:
+            return '该患者在{}年内预测不会患糖尿病'.format(round(times[-1],2))
+            
+        else:
+            for time in times:
+                if fn(time)>1:
+                    time_value=time
+                    break
+                return '该患者将在{}年时患糖尿病'.format(time_value)
+                
+
+@app.post('/diabetes_pred_proba')
+def diabetes_pred_proba(input_parameters:model_input,time: int):
+    input_data=input_parameters.json()
+    input_dictionary=json.loads(input_data)
+    patient=pd.DataFrame(input_dictionary,index=[0])
+    chf_funcs=diabetes_model.predict_cumulative_hazard_function(patient)
+    for fn in chf_funcs:
+        result=fn(time)
+        result=round(result,3)*100
+        return '该患者在{}年内患糖尿病的概率是{}%'.format(time,result)
+        
     
-    preg = input_dictionary['Pregancies']
-    glu = input_dictionary['Glucose']
-    bp = input_dictionary['BloodPressure']
-    skin = input_dictionary['SkinThickness']
-    insulin = input_dictionary['Insulin']
-    bmi = input_dictionary['BMI']
-    dpf = input_dictionary['DiabetesPedigreeFunction']
-    age = input_dictionary['Age']
+
+if __name__ == '__main__':
+    uvicorn.run(app="main:app", host="127.0.0.1", port=8000)
     
-    input_list = [preg, glu, bp, skin, insulin, bmi, dpf, age]
     
-    prediction = diabetes_model.predict([input_list])
-    
-    if prediction[0] == 0:
-        return 'The person is not diabetic'
-    else:
-        return 'The person is diabetic'
